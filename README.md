d8b318d8f8537a2b5987366165d07a600bce3ec6
kacperduras <git@kacperduras.pl>
2018-05-01 14:46:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CommandBridge [![Build Status](https://travis-ci.org/kacperduras/Bridgy.svg?branch=master)](https://travis-ci.org/kacperduras/Bridgy)
+Bridgy is an example implementation of [CommandBridge](https://github.com/kacperduras/CommandBridge) plugin.
+
+## Releases
+Stable releases: [here](https://github.com/kacperduras/Bridgy/releases)  
+Development releases: [here](https://ci.kacperduras.pl/job/Bridgy)
+
+## Contributing
+
+Feel free to submit pull requests, just make sure your changes are consistent with
+[Google's Kotlin code style](https://android.github.io/kotlin-guides/style.html).
+
+## License
+[The Apache License, Version 2.0](LICENSE)
---
+++ src/main/kotlin/pl.kacperduras.bridgy/BridgyPlugin.kt
@@ -0,0 +1,52 @@
+package pl.kacperduras.bridgy
+
+import org.apache.commons.lang3.StringUtils
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import pl.kacperduras.commandbridge.shaded.CommandBridgeAPI
+
+class BridgyPlugin : JavaPlugin() {
+
+  val COMMAND_BRIDGE_PLUGIN_NAME = "commandbridge-bukkit"
+
+  private lateinit var bridge: CommandBridgeAPI
+
+  override fun onEnable() {
+    if (!this.server.pluginManager.isPluginEnabled(COMMAND_BRIDGE_PLUGIN_NAME)) {
+      this.isEnabled = false
+      return
+    }
+
+    this.bridge = this.server.pluginManager.getPlugin(COMMAND_BRIDGE_PLUGIN_NAME) as CommandBridgeAPI
+    this.getCommand("bridgy").executor = this
+  }
+
+  override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+    if (args.isEmpty()) {
+      sender.sendMessage(this.color("&cCorrect usage: &7/bridgy <command>&c."))
+      return true
+    }
+
+    if (!bridge.isBukkit) {
+      sender.sendMessage(this.color("&cInternal error!"))
+      return true
+    }
+
+    if (sender is Player) {
+      val player: Player = sender
+      bridge.bungee(player.name, StringUtils.join(args, " "))
+      return true
+    }
+
+    sender.sendMessage(this.color("&cYou must be a Player!"))
+    return true
+  }
+
+  private fun color(string: String): String {
+    return ChatColor.translateAlternateColorCodes('&', string)
+  }
+
+}
